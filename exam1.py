896e3fd88843644d22938e7f632db1f93302e9df
josefinestrecker <qnf489@alumni.ku.dk>
2023-05-25 14:34:18 +0200

    Ny

--- exam1.py
+++ exam1.py
@@ -43,21 +43,23 @@ def negative_utility(tau):
 # Define the new utility function
 def utility_1(L, G, alpha, sigma, rho, v, epsilon):
     C = kappa + (1 + tau) * w * L
-    return ((alpha * C**((sigma - 1) / sigma) + (1 - alpha) * G**((sigma - 1) / sigma))**((1 - rho) / sigma) - 1) / (1 - rho) - v * L**(1 + epsilon) / (1 + epsilon)
+    return (((alpha * C**((sigma - 1) / sigma) + (1 - alpha) * G**((sigma-1) / (sigma)))**((sigma / (sigma-1)))**((1 - rho)) - 1) / (1 - rho)) - v * L**(1 + epsilon) / (1 + epsilon)
 
 # Define the function for the equilibrium condition
 def equilibrium(G, w_tilde, alpha, sigma, rho, v, epsilon):
     L = optimal_labor_supply(w_tilde, kappa, alpha, v)
-    return G - tau * w * L
+    return G - tau * w_tilde * L
+
 
 def negative_utility_with_equilibrium(tau, w, alpha, sigma, rho, v, epsilon):
     w_tilde = (1 - tau) * w
     G = fsolve(equilibrium, 1.0, args=(w_tilde, alpha, sigma, rho, v, epsilon))
     L = optimal_labor_supply(w_tilde, kappa, alpha, v)
-    return -utility_2(L, G, alpha, sigma, rho, v, epsilon)
+    return -utility_1(L, G, alpha, sigma, rho, v, epsilon)
 
 
-#Problem 2
+
+
 # Set up the parameters
 eta_2 = 0.5
 w_2 = 1.0
@@ -69,11 +71,10 @@ R_2 = (1 + 0.01)**(1/12)
 K_2 = 10000  # Number of random shock series
 T_2 = 120  # Number of periods
 
-
 # Define the function for optimal labor supply choice
-def optimal_labor_supply(kappa_2, eta_2, w_2):
+def optimal_labor_supply_2(kappa_2, eta_2, w_2):
     return ((1 - eta_2) * kappa_2 / w_2)**(1 / eta_2)
 
 # Define the function for profits
-def profits(kappa_2, l_2, eta_2, w_2):
+def profits_2(kappa_2, l_2, eta_2, w_2):
     return kappa_2 * l_2**(1 - eta_2) - w_2 * l_2
